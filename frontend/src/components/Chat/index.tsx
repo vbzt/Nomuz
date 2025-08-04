628f483564bee58117ca6450d1c5f53afe54c4c3
Victor Henrique. <123006884+vhnb@users.noreply.github.com>
2025-08-03 22:53:04 -0300

    Add files via upload

---
+++ frontend/src/components/Chat/index.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3001')
+
+export default function Chat() {
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState<string[]>([])
+
+  useEffect(() => {
+    socket.on('message', (msg: string) => {
+      setMessages((prev) => [...prev, msg])
+    })
+
+    return () => {
+      socket.off('message')
+    }
+  }, [])
+
+  const sendMessage = () => {
+    if (message.trim()) {
+      socket.emit('message', message)
+      setMessage('')
+    }
+  }
+
+  return (
+    <div>
+      <h1>💬 Chat Teste</h1>
+      <div>
+        {messages.map((msg, idx) => (
+          <div key={idx} className="mb-1">{msg}</div>
+        ))}
+      </div>
+      <div>
+        <input value={message} onChange={(e) => setMessage(e.target.value)} onKeyDown={(e) => e.key === 'Enter' && sendMessage()} placeholder="Digite sua mensagem..." />
+        <button onClick={sendMessage}>Enviar</button>
+      </div>
+    </div>
+  )
+}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Chat from '@/components/Chat';
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-zinc-800 flex items-center justify-center">
+      <Chat />
+    </main>
+  )
+}
