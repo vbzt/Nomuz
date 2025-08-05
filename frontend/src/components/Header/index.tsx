cd80191d2a0b3ad99cd6e9de152d78f0bbf6656f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-05 10:50:03 -0300

    feat: quase nada

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image"
+import icon from '../../../public/icon.png'
+import { Button } from "@/components/ui/button"
+
 export default function Header() {
-    return(
-        <header>
-            <div>
-                
+    return (
+        <header className="flex items-center justify-center h-[80px] sticky top-[0]">
+            <div className="flex items-center justify-between max-w-[1100px] w-[90%]">
+                <Image className="h-[50px] w-[50px]" src={icon} alt="Icon" />
             </div>
         </header>
     )
