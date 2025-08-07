2de66fa0e2c7d479c210213f49cdef0d70a7df27
vhnb <labsfiap@fiap.com.br>
2025-08-07 09:50:16 -0300

    feat: Adding animations

--- frontend/src/pages/auth/login/index.tsx
+++ frontend/src/pages/auth/login/index.tsx
@@ -10,7 +10,7 @@ export default function Login() {
             backgroundImage: `linear-gradient(#2727271c 1px, transparent 1px), linear-gradient(90deg, #2727271c 1px, transparent 1px)`,
             backgroundSize: '80px 80px',
         }} className="flex flex-col min-h-[calc(100vh-65px)] items-center justify-start md:justify-center">
-            
+
 
             <div className='flex items-start justify-center flex-col max-w-[400px] w-[90%]'>
                 <h1 className="mt-[20px] mb-[5px] scroll-m-20 text-center text-[24px] font-bold tracking-tight text-balance">Acesse sua conta</h1>
@@ -26,6 +26,10 @@ export default function Login() {
                             <a href="/" className="text-[12px]">Esqueceu sua senha?</a>
                         </div>
                         <Input type="password" id="senha" placeholder="suasenha123" className="bg-[#0c0c13]" />
+                    </div>
+                    <div className="flex items-center gap-3 w-full mb-[20px]">
+                        <Checkbox id="terms" />
+                        <Label htmlFor="terms">Permanecer conectado</Label>
                     </div>
                     <Button className="mb-[20px] w-full bg-[#36577d] hover:bg-[#254161]" type="submit">Entrar</Button>
                     <p className="text-[14px] mb-[20px]">Não possui uma conta? <a className="underline" href="/auth/register">Crie uma</a></p>
