5fdbdbb8417f7dcbef3f4c00831e464663887ca2
vbzt <vitorcastrobuzato@gmail.com>
2025-08-07 09:55:17 -0300

    feat: implement complete password recovery system

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,11 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ForgotPasswordDTO } from './dto/forgot-password.dto';
 import { AuthService } from './auth.service';
+import { RegisterUserDTO } from './dto/register-user.dto';
+import { LoginDTO } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
 
   constructor( private readonly authService: AuthService){}
+
+  @Post('/register')
+  async register(@Body() data: RegisterUserDTO){ 
+    return this.authService.register(data)
+  }
+
+  @Post('/login')
+  async login(@Body() data: LoginDTO){
+    return this.authService.login(data)
+  }
 
   @Post('/forgot')   
   async forgotPassword(@Body() data: ForgotPasswordDTO){  
