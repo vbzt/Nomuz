8e308613d13193dd7e6409fe6a0096714a03099d
vhnb <victorredin122@gmail.com>
2025-08-08 13:03:58 -0300

    refactor: Header Improvements

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -15,11 +15,11 @@ export default function Header() {
                     <CustomNavigationMenu />
                 </div>
                 <div className="flex items-center justify-center flex-row">
-                    <button onClick={() => window.location.href = '/auth/login'} className="hidden md:flex items-center justify-between p-[6px] rounded-[10px] border border-[#15151e] ml-[10px] h-9 pl-[15px] pr-[15px] transition duration-[0.2s] cursor-pointer ease-in-out hover:bg-[#ffffff0a]">
-                        <TbUser size={16} className="transition duration-[0.2s] ease-in-out text-[#b3b3b3] hover:text-[#fff]" />
-                        <p className="text-[14px] ml-[10px]">Iniciar sessão</p>
+                    <button onClick={() => window.location.href = '/auth/login'} className="hidden md:flex items-center justify-between p-[6px] rounded-[10px] border border-[#15151e] ml-[10px] h-9 pl-[15px] pr-[15px] transition duration-[0.2s] cursor-pointer ease-in-out hover:bg-[#ffffff0a] group">
+                        <TbUser size={16} className="transition duration-[0.2s] ease-in-out text-[#b3b3b3] group-hover:text-[#fff]" />
+                        <p className="text-[14px] ml-[10px] text-[#b3b3b3] transition duration-[0.2s] ease-in-out group-hover:text-[#fff]">Iniciar sessão</p>
                     </button>
-                    <button className="hidden md:flex justify-center items-center p-[6px] rounded-[10px] border border-[#15151e] ml-[10px] h-9 w-9 transition duration-[0.2s] cursor-pointer ease-in-out hover:bg-[#ffffff0a]"><TbSearch size={18} className="transition duration-[0.2s] ease-in-out text-[#b3b3b3] hover:text-[#fff]" /></button>
+                    <button className="hidden md:flex justify-center items-center p-[6px] rounded-[10px] border border-[#15151e] ml-[10px] h-9 w-9 transition duration-[0.2s] cursor-pointer ease-in-out hover:bg-[#ffffff0a] group"><TbSearch size={18} className="transition duration-[0.2s] ease-in-out text-[#b3b3b3] group-hover:text-[#fff]" /></button>
                 </div>
                 <MenuMobile />
             </div>
