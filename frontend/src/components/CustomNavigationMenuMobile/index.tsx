3c9cd2f9c17c1e864266553cf7ea7c225cd982d1
victor <victorredin122@gmail.com>
2025-08-10 23:02:25 -0300

    refactor: Revamped mobile menu + finishes

---
+++ frontend/src/components/CustomNavigationMenuMobile/index.tsx
@@ -0,0 +1,54 @@
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+import { TbBook2 } from "react-icons/tb";
+
+export default function CustomNavigationMenuMobile() {
+    const itemsServices = [
+        {
+            title: 'Teste',
+            desc: 'É apenas um teste',
+            icon: <TbBook2 />,
+            link: '/'
+        },
+        {
+            title: 'Teste',
+            desc: 'É apenas um teste',
+            icon: <TbBook2 />,
+            link: '/'
+        }
+    ]
+
+    function handleNavigate(link: string) {
+        window.location.href = `${link}`
+    }
+
+    return (
+        <>
+            <a className="w-full text-[15px] border border-[#15151e] p-[8px] rounded-[10px] mb-[10px]" href="/">Inicio</a>
+            <Accordion type="single" collapsible className="w-full" defaultValue="item-1">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger className="w-full text-[15px] border border-[#15151e] p-[8px] rounded-[10px] mb-[10px]">Product Information</AccordionTrigger>
+                    <AccordionContent className="flex flex-col gap-4 text-balance">
+                        <ul className="flex items-center justify-center p-[0px] flex-col">
+                            {itemsServices.map((item) => (
+                                <li onClick={() => handleNavigate(item.link)} className="flex items-center justify-start p-1 rounded-[10px] flex-row w-[100%] hover:bg-[#36577d19] transition duration-[0.2s] cursor-pointer ease-in-out mb-[8px] last:mb-[0px]">
+                                    <p className="text-[30px] mr-[10px] text-[#aaaaaa] p-[5px] bg-[#5757570a] rounded-[10px] border border-[#15151e]">{item.icon}</p>
+                                    <div className="flex items-start justify-start flex-col">
+                                        <h1 className="text-[14px] text-[#fff]">{item.title}</h1>
+                                        <p className="text-[10px] text-[#b3b3b3]">{item.desc}</p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+            <a className="w-full text-[15px] border border-[#15151e] p-[8px] rounded-[10px] mb-[10px]" href="/">Blog</a>
+            <a className="w-full text-[15px] border border-[#15151e] p-[8px] rounded-[10px] mb-[10px]" href="/">Termos</a>
+        </>
+    )
+}
--- frontend/src/components/MenuMobile/index.tsx
+++ frontend/src/components/MenuMobile/index.tsx
@@ -11,6 +11,9 @@ import { TbMenu } from "react-icons/tb";
 import { IoCloseOutline } from "react-icons/io5";
 import { TbSearch } from "react-icons/tb";
 import { TbUser } from "react-icons/tb";
+import Image from "next/image"
+import icon from '../../../public/icon.png'
+import CustomNavigationMenuMobile from "../CustomNavigationMenuMobile";
 
 export default function MenuMobile() {
     return (
@@ -19,17 +22,21 @@ export default function MenuMobile() {
                 <button className="block p-2 rounded-[10px] border border-[#15151e] md:hidden"><TbMenu size={20} /></button>
             </SheetTrigger>
             <SheetContent side="left">
-                <SheetHeader className="flex h-[65px] items-center justify-between flex-row-reverse px-4 border-b border-[#15151e]">
+                <SheetHeader className="flex h-[65px] items-center justify-between flex-row px-4 border-b border-[#15151e]">
+                    <Image className="h-[50px] w-[50px] mr-[15px]" src={icon} alt="Icon" />
                     <SheetClose asChild>
                         <button className="p-2 rounded-[10px] border border-[#15151e]"><IoCloseOutline size={20} /></button>
                     </SheetClose>
                 </SheetHeader>
                 <div className="px-4 flex justify-center items-center">
-                    <button onClick={() => window.location.href = '/auth/login'} className="w-full flex items-center justify-start p-2 rounded-[10px] border border-[#15151e] transition duration-[0.2s] cursor-pointer ease-in-out hover:bg-[#ffffff0a]">
+                    <button onClick={() => window.location.href = '/auth/login'} className="w-full flex items-center justify-start bg-[#5757570a] p-[8px] rounded-[10px] border border-[#15151e] transition duration-[0.2s] cursor-pointer ease-in-out hover:bg-[#ffffff0a]">
                         <TbUser size={20} className="transition duration-[0.2s] ease-in-out text-[#b3b3b3] hover:text-[#fff]" />
                         <p className="text-[14px] ml-[10px]">Iniciar sessão</p>
                     </button>
-                    <button className="block p-2 rounded-[10px] border border-[#15151e] ml-[10px]"><TbSearch size={20} className="transition duration-[0.2s] ease-in-out text-[#b3b3b3] hover:text-[#fff]" /></button>
+                    <button className="block p-[8px] rounded-[10px] border border-[#15151e] bg-[#5757570a] ml-[10px]"><TbSearch size={20} className="transition duration-[0.2s] ease-in-out text-[#b3b3b3] hover:text-[#fff]" /></button>
+                </div>
+                <div className="flex items-start justify-center flex-col px-4">
+                    <CustomNavigationMenuMobile />
                 </div>
             </SheetContent>
         </Sheet>
