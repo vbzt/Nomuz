eadea96671614ac0f30f64ef07f811d06de340b7
vbzt <vitorcastrobuzato@gmail.com>
2025-08-19 11:40:04 -0300

    refactor: change err lang

--- api/src/common/decorators/user.decorator.ts
+++ api/src/common/decorators/user.decorator.ts
@@ -1,9 +1,9 @@
 import { createParamDecorator, ExecutionContext, NotFoundException } from "@nestjs/common";
 
-export const User = createParamDecorator((args: string, context: ExecutionContext) => { 
+export const UserData = createParamDecorator((args: string, context: ExecutionContext) => { 
   const request = context.switchToHttp().getRequest()
   const userData = request.userData
-  if(!userData) throw new NotFoundException("User not found.")
+  if(!userData) throw new NotFoundException("Este usuário não existe.")
   if(args) return userData[args]
   return userData
 })
