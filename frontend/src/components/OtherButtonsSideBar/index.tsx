88f813941335c080c7f550e870ab0f5be8fe28b2
vhnb <victorredin122@gmail.com>
2025-08-25 12:18:04 -0300

    refactor: added the user account container to the sidebar

--- frontend/src/components/OtherButtonsSideBar/index.tsx
+++ frontend/src/components/OtherButtonsSideBar/index.tsx
@@ -11,6 +11,23 @@ import {
     TooltipContent,
     TooltipTrigger,
 } from "@/components/ui/tooltip"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { TbLogout } from "react-icons/tb";
+import Image from "next/image";
+import imageteste from '../../../public/image.jpg'
 
 export default function SidebarActions() {
     const { state } = useSidebar()
@@ -52,6 +69,24 @@ export default function SidebarActions() {
             <SidebarTrigger className="flex justify-center items-center bg-[#0c0c13] p-[6px] rounded-[10px] border border-[#15151e] h-9 w-9 transition duration-[0.2s] cursor-pointer ease-in-out hover:bg-[#ffffff0a] hover:text-[#fff] group" />
             {state === 'collapsed' && (
                 <>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button className="mb-[8px] flex justify-center items-center bg-[#0c0c13] p-[2px] rounded-[10px] border border-[#15151e] h-9 w-9 transition duration-[0.2s] cursor-pointer ease-in-out hover:bg-[#ffffff0a] hover:text-[#fff] group">
+                                <Image className="rounded-[10px]" src={imageteste} alt="teste" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-[238.5px]" align="start">
+                            <DropdownMenuLabel className="text-[#b3b3b3] text-[12px] select-none">Minha conta</DropdownMenuLabel>
+                            <DropdownMenuItem className="text-[#b3b3b3] hover:bg-[#36577d19] hover:text-[#fff] transition duration-[0.2s] ease-in-out cursor-pointer" onClick={() => window.location.href = '/'}>Início</DropdownMenuItem>
+                            <DropdownMenuItem className="text-[#b3b3b3] hover:bg-[#36577d19] hover:text-[#fff] transition duration-[0.2s] ease-in-out cursor-pointer" onClick={() => window.location.href = '/dashboard/settings'}>Configurações</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem className="text-[#c72e38] hover:bg-[#c72e38] hover:text-[#fff] transition duration-[0.2s] cursor-pointer ease-in-out group">
+                                Log out
+                                <TbLogout className="text-[#c72e38] group-hover:text-[#fff] transition duration-[0.2s] ease-in-out" />
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <div className="w-full p-[0.5px] h-[0.5px] mb-[8px] bg-[#15151e]"></div>
                     {props.map((item) => (
                         <Tooltip>
                             <TooltipTrigger asChild>
